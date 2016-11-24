faaabd3f65de8afcb0fa58bf42a62ebc47556e61
Jerry Wang <Jerryw.wang@emerson.com>
2016-11-24 14:29:58 +0800

    no message

--- HMIModuleCode-T2/Interface/Settings/SysConfiguration.cpp
+++ HMIModuleCode-T2/Interface/Settings/SysConfiguration.cpp
@@ -1,80 +1,81 @@
 #include "SysConfiguration.h"
-Status_Data Status_Data::operator=(const Status_Data &StatusDataObj) const
+Status_Data &Status_Data::operator= (const Status_Data &StatusDataObj)
 {
-    Status_Data Obj;
-    Obj.RevCode = StatusDataObj.RevCode;
-    Obj.CreatedDate = StatusDataObj.CreatedDate;
-    Obj.OperatorName = StatusDataObj.OperatorName;
+    if(this == &StatusDataObj)
+        return *this;
+    this->RevCode = StatusDataObj.RevCode;
+    this->CreatedDate = StatusDataObj.CreatedDate;
+    this->OperatorName = StatusDataObj.OperatorName;
 
-    Obj.Soft_Settings.Lang_Support = StatusDataObj.Soft_Settings.Lang_Support;
-    Obj.Soft_Settings.Horn_Calibrate = StatusDataObj.Soft_Settings.Horn_Calibrate;
-    Obj.Soft_Settings.AutoPreburst = StatusDataObj.Soft_Settings.AutoPreburst;
+    this->Soft_Settings.Lang_Support = StatusDataObj.Soft_Settings.Lang_Support;
+    this->Soft_Settings.Horn_Calibrate = StatusDataObj.Soft_Settings.Horn_Calibrate;
+    this->Soft_Settings.AutoPreburst = StatusDataObj.Soft_Settings.AutoPreburst;
 //    Obj.Soft_Settings.AutoGetNext = StatusDataObj.Soft_Settings.AutoGetNext;
-    Obj.Soft_Settings.SonicGenWatts = StatusDataObj.Soft_Settings.SonicGenWatts;
-    Obj.Soft_Settings.ToolCoverIgnore = StatusDataObj.Soft_Settings.ToolCoverIgnore;
-    Obj.Soft_Settings.Pressure2Unit = StatusDataObj.Soft_Settings.Pressure2Unit;
-    Obj.Soft_Settings.Mm2Awg = StatusDataObj.Soft_Settings.Mm2Awg;
-    Obj.Soft_Settings.PWWidth2Height = StatusDataObj.Soft_Settings.PWWidth2Height;
-    Obj.Soft_Settings.MinAmplitude = StatusDataObj.Soft_Settings.MinAmplitude;
-    Obj.Soft_Settings.MinPressure = StatusDataObj.Soft_Settings.MinPressure;
+    this->Soft_Settings.SonicGenWatts = StatusDataObj.Soft_Settings.SonicGenWatts;
+    this->Soft_Settings.ToolCoverIgnore = StatusDataObj.Soft_Settings.ToolCoverIgnore;
+    this->Soft_Settings.Pressure2Unit = StatusDataObj.Soft_Settings.Pressure2Unit;
+    this->Soft_Settings.Mm2Awg = StatusDataObj.Soft_Settings.Mm2Awg;
+    this->Soft_Settings.PWWidth2Height = StatusDataObj.Soft_Settings.PWWidth2Height;
+    this->Soft_Settings.MinAmplitude = StatusDataObj.Soft_Settings.MinAmplitude;
+    this->Soft_Settings.MinPressure = StatusDataObj.Soft_Settings.MinPressure;
 //    Obj.Soft_Settings.GotoRunScreen = StatusDataObj.Soft_Settings.GotoRunScreen;
 //    Obj.Soft_Settings.NoToolCover4SU = StatusDataObj.Soft_Settings.NoToolCover4SU;
-    Obj.Soft_Settings.WeldFormula = StatusDataObj.Soft_Settings.WeldFormula;
-    Obj.Soft_Settings.RunCount = StatusDataObj.Soft_Settings.RunCount;
-    Obj.Soft_Settings.Teach_Mode = StatusDataObj.Soft_Settings.Teach_Mode;
-    Obj.Soft_Settings.TunePoint = StatusDataObj.Soft_Settings.TunePoint;
-    Obj.Soft_Settings.FrequencyOffset = StatusDataObj.Soft_Settings.FrequencyOffset;
+    this->Soft_Settings.WeldFormula = StatusDataObj.Soft_Settings.WeldFormula;
+    this->Soft_Settings.RunCount = StatusDataObj.Soft_Settings.RunCount;
+    this->Soft_Settings.Teach_Mode = StatusDataObj.Soft_Settings.Teach_Mode;
+    this->Soft_Settings.TunePoint = StatusDataObj.Soft_Settings.TunePoint;
+    this->Soft_Settings.FrequencyOffset = StatusDataObj.Soft_Settings.FrequencyOffset;
 
-    Obj.Cust_Data.mod10a_settings.UnsignedWeldData =
+    this->Cust_Data.mod10a_settings.UnsignedWeldData =
             StatusDataObj.Cust_Data.mod10a_settings.UnsignedWeldData;
-    Obj.Cust_Data.mod10a_settings.AfterBurstDelay =
+    this->Cust_Data.mod10a_settings.AfterBurstDelay =
             StatusDataObj.Cust_Data.mod10a_settings.AfterBurstDelay;
-    Obj.Cust_Data.mod10a_settings.AfterBurstDuration =
+    this->Cust_Data.mod10a_settings.AfterBurstDuration =
             StatusDataObj.Cust_Data.mod10a_settings.AfterBurstDuration;
-    Obj.Cust_Data.mod10a_settings.EnergyWindowPL =
+    this->Cust_Data.mod10a_settings.EnergyWindowPL =
             StatusDataObj.Cust_Data.mod10a_settings.EnergyWindowPL;
-    Obj.Cust_Data.mod10a_settings.PreBurstIndex =
+    this->Cust_Data.mod10a_settings.PreBurstIndex =
             StatusDataObj.Cust_Data.mod10a_settings.PreBurstIndex;
     for(int i = 0; i < WIDTH_ADJ; i++)
-        Obj.Cust_Data.cust_qual_range[i] = StatusDataObj.Cust_Data.cust_qual_range[i];
+        this->Cust_Data.cust_qual_range[i] = StatusDataObj.Cust_Data.cust_qual_range[i];
 
     for(int i = 0; i< END_SL; i++)
     {
-        Obj.HSDATA.HARD_LIMIT[i].TypeHS =
+        this->HSDATA.HARD_LIMIT[i].TypeHS =
                 StatusDataObj.HSDATA.HARD_LIMIT[i].TypeHS;
-        Obj.HSDATA.HARD_LIMIT[i].ValueHS =
+        this->HSDATA.HARD_LIMIT[i].ValueHS =
                 StatusDataObj.HSDATA.HARD_LIMIT[i].ValueHS;
-        Obj.HSDATA.SOFT_LIMIT[i].TypeHS =
+        this->HSDATA.SOFT_LIMIT[i].TypeHS =
                 StatusDataObj.HSDATA.SOFT_LIMIT[i].TypeHS;
-        Obj.HSDATA.SOFT_LIMIT[i].ValueHS =
+        this->HSDATA.SOFT_LIMIT[i].ValueHS =
                 StatusDataObj.HSDATA.SOFT_LIMIT[i].ValueHS;
     }
 
 
-    Obj.ComInfo.BaudRate = StatusDataObj.ComInfo.BaudRate;
-    Obj.ComInfo.COMport = StatusDataObj.ComInfo.COMport;
-    Obj.MachineType = StatusDataObj.MachineType;
-    Obj.MachineDate = StatusDataObj.MachineDate;
-    Obj.KeepDailyHistory = StatusDataObj.KeepDailyHistory;
+    this->ComInfo.BaudRate = StatusDataObj.ComInfo.BaudRate;
+    this->ComInfo.COMport = StatusDataObj.ComInfo.COMport;
+    this->MachineType = StatusDataObj.MachineType;
+    this->MachineDate = StatusDataObj.MachineDate;
+    this->KeepDailyHistory = StatusDataObj.KeepDailyHistory;
 
     for (int i = 0; i< PERMISSIONLEVEL; i++)
     {
-        Obj.PasswordData[i].Identifier = StatusDataObj.PasswordData[i].Identifier;
-        Obj.PasswordData[i].Password = StatusDataObj.PasswordData[i].Password;
-        Obj.PasswordData[i].PWPermissions = StatusDataObj.PasswordData[i].PWPermissions;
+        this->PasswordData[i].Identifier = StatusDataObj.PasswordData[i].Identifier;
+        this->PasswordData[i].Password = StatusDataObj.PasswordData[i].Password;
+        this->PasswordData[i].PWPermissions = StatusDataObj.PasswordData[i].PWPermissions;
     }
-    Obj.CurrentFunIndex.clear();
+    this->CurrentFunIndex.clear();
     for(int i = 0; i < CurrentFunIndex.size(); i++)
     {
-        Obj.CurrentFunIndex.insert(i, StatusDataObj.CurrentFunIndex.at(i));
+        this->CurrentFunIndex.insert(i, StatusDataObj.CurrentFunIndex.at(i));
     }
 
     for (int i = 0; i< FormulaRangSize; i++)
     {
-        Obj.WeldSettings4Build[i].MaxRange = StatusDataObj.WeldSettings4Build[i].MaxRange;
-        Obj.WeldSettings4Build[i].MinRange = StatusDataObj.WeldSettings4Build[i].MinRange;
-        Obj.WeldSettings4Build[i].Multplier = StatusDataObj.WeldSettings4Build[i].Multplier;
-        Obj.WeldSettings4Build[i].Offset = StatusDataObj.WeldSettings4Build[i].Offset;
+        this->WeldSettings4Build[i].MaxRange = StatusDataObj.WeldSettings4Build[i].MaxRange;
+        this->WeldSettings4Build[i].MinRange = StatusDataObj.WeldSettings4Build[i].MinRange;
+        this->WeldSettings4Build[i].Multplier = StatusDataObj.WeldSettings4Build[i].Multplier;
+        this->WeldSettings4Build[i].Offset = StatusDataObj.WeldSettings4Build[i].Offset;
     }
 
 //    for (int i = 0; i< WDSI_SIZE; i++)
@@ -83,74 +84,74 @@ Status_Data Status_Data::operator=(const Status_Data &StatusDataObj) const
 //    Obj.WeldSettingDefaultTrigPress = StatusDataObj.WeldSettingDefaultTrigPress;
 //    Obj.AutoStartLastPart = StatusDataObj.AutoStartLastPart;
 //    Obj.NRGtoHeightMode = StatusDataObj.NRGtoHeightMode;
-    Obj.ShrinkTubeMode = StatusDataObj.ShrinkTubeMode;
+    this->ShrinkTubeMode = StatusDataObj.ShrinkTubeMode;
     ShrinkTubeData tmpShrinkTube;
-    Obj.ShrinkTubeDefaults.clear();
+    this->ShrinkTubeDefaults.clear();
     for(int i = 0; i < StatusDataObj.ShrinkTubeDefaults.size(); i++)
     {
         tmpShrinkTube.Name = StatusDataObj.ShrinkTubeDefaults[i].Name;
         tmpShrinkTube.temp = StatusDataObj.ShrinkTubeDefaults[i].temp;
         tmpShrinkTube.Time = StatusDataObj.ShrinkTubeDefaults[i].Time;
-        Obj.ShrinkTubeDefaults.insert(i, tmpShrinkTube);
+        this->ShrinkTubeDefaults.insert(i, tmpShrinkTube);
     }
 
-    Obj.RemoteRecallport = StatusDataObj.RemoteRecallport;
-    Obj.SoftLimitsModeFlags = StatusDataObj.SoftLimitsModeFlags;
-    Obj.SoftLimitSampleSize = StatusDataObj.SoftLimitSampleSize;
+    this->RemoteRecallport = StatusDataObj.RemoteRecallport;
+    this->SoftLimitsModeFlags = StatusDataObj.SoftLimitsModeFlags;
+    this->SoftLimitSampleSize = StatusDataObj.SoftLimitSampleSize;
     for(int i = 0; i < SLIControlLimitSize; i++)
         for(int j = 0; j < SLI_Size; j++)
-            Obj.SoftLimit[i][j] = StatusDataObj.SoftLimit[i][j];
+            this->SoftLimit[i][j] = StatusDataObj.SoftLimit[i][j];
 
 //    Obj.QualityLimitsModeFlags = StatusDataObj.QualityLimitsModeFlags;
-    Obj.FileSystemFlags = StatusDataObj.FileSystemFlags;
-    Obj.AutoGetNextDelay = StatusDataObj.AutoGetNextDelay;
-    Obj.NetworkingEnabled = StatusDataObj.NetworkingEnabled;
-    Obj.RemoteDataLogging = StatusDataObj.RemoteDataLogging;
-    Obj.CurrentWrkStaID = StatusDataObj.CurrentWrkStaID;
-    Obj.CentralComputerID = StatusDataObj.CentralComputerID;
-    Obj.ActuatorMode = StatusDataObj.ActuatorMode;
-    Obj.AntisideSpliceTime = StatusDataObj.AntisideSpliceTime;
-    Obj.CurrentCoolingDur = StatusDataObj.CurrentCoolingDur;
-    Obj.CurrentCoolingDel = StatusDataObj.CurrentCoolingDel;
-    Obj.CurrentCoolingMode = StatusDataObj.CurrentCoolingMode;
-    Obj.CurrentCoolingTooling = StatusDataObj.CurrentCoolingTooling;
-    Obj.LockonAlarm = StatusDataObj.LockonAlarm;
-    Obj.RunMode = StatusDataObj.RunMode;
+    this->FileSystemFlags = StatusDataObj.FileSystemFlags;
+    this->AutoGetNextDelay = StatusDataObj.AutoGetNextDelay;
+    this->NetworkingEnabled = StatusDataObj.NetworkingEnabled;
+    this->RemoteDataLogging = StatusDataObj.RemoteDataLogging;
+    this->CurrentWrkStaID = StatusDataObj.CurrentWrkStaID;
+    this->CentralComputerID = StatusDataObj.CentralComputerID;
+    this->ActuatorMode = StatusDataObj.ActuatorMode;
+    this->AntisideSpliceTime = StatusDataObj.AntisideSpliceTime;
+    this->CurrentCoolingDur = StatusDataObj.CurrentCoolingDur;
+    this->CurrentCoolingDel = StatusDataObj.CurrentCoolingDel;
+    this->CurrentCoolingMode = StatusDataObj.CurrentCoolingMode;
+    this->CurrentCoolingTooling = StatusDataObj.CurrentCoolingTooling;
+    this->LockonAlarm = StatusDataObj.LockonAlarm;
+    this->RunMode = StatusDataObj.RunMode;
     for(int i = 0; i< 4; i++)
-        Obj.Machineflags.Word[i] = StatusDataObj.Machineflags.Word[i];
+        this->Machineflags.Word[i] = StatusDataObj.Machineflags.Word[i];
 
-    Obj.CycleCount = StatusDataObj.CycleCount;
+    this->CycleCount = StatusDataObj.CycleCount;
     for(int i = 0; i< 8; i++)
     {
-        Obj.MaintenanceLimits[i] = StatusDataObj.MaintenanceLimits[i];
-        Obj.CurrentMaintenanceLimits[i] = StatusDataObj.CurrentMaintenanceLimits[i];
-        Obj.MaintenanceDateStarted[i] = StatusDataObj.MaintenanceDateStarted[i];
+        this->MaintenanceLimits[i] = StatusDataObj.MaintenanceLimits[i];
+        this->CurrentMaintenanceLimits[i] = StatusDataObj.CurrentMaintenanceLimits[i];
+        this->MaintenanceDateStarted[i] = StatusDataObj.MaintenanceDateStarted[i];
     }
-    Obj.TubeShrinkerport = StatusDataObj.TubeShrinkerport;
-    Obj.HistoryGraphData = StatusDataObj.HistoryGraphData;
-    Obj.RemoteGraphData = StatusDataObj.RemoteGraphData;
-    Obj.StartScreen = StatusDataObj.StartScreen;
-    Obj.EnableModularFlag = StatusDataObj.EnableModularFlag;
+    this->TubeShrinkerport = StatusDataObj.TubeShrinkerport;
+    this->HistoryGraphData = StatusDataObj.HistoryGraphData;
+    this->RemoteGraphData = StatusDataObj.RemoteGraphData;
+    this->StartScreen = StatusDataObj.StartScreen;
+    this->EnableModularFlag = StatusDataObj.EnableModularFlag;
     for(int i = 0; i< PASSCOUNT; i++)
     {
-        Obj.ModularPassword[i].Identifier = StatusDataObj.ModularPassword[i].Identifier;
-        Obj.ModularPassword[i].Password = StatusDataObj.ModularPassword[i].Password;
+        this->ModularPassword[i].Identifier = StatusDataObj.ModularPassword[i].Identifier;
+        this->ModularPassword[i].Password = StatusDataObj.ModularPassword[i].Password;
     }
 
-    Obj.GraphSampleRatio = StatusDataObj.GraphSampleRatio;
-    Obj.GraphDataLen = StatusDataObj.GraphDataLen;
-    Obj.CutoffMode = StatusDataObj.CutoffMode;
-    Obj.LockKeyFlag = StatusDataObj.LockKeyFlag;
-    Obj.FootPedalFlag = StatusDataObj.FootPedalFlag;
-    Obj.ServerPort = StatusDataObj.ServerPort;
-    Obj.ModularProductionEnabled = StatusDataObj.ModularProductionEnabled;
+    this->GraphSampleRatio = StatusDataObj.GraphSampleRatio;
+    this->GraphDataLen = StatusDataObj.GraphDataLen;
+    this->CutoffMode = StatusDataObj.CutoffMode;
+    this->LockKeyFlag = StatusDataObj.LockKeyFlag;
+    this->FootPedalFlag = StatusDataObj.FootPedalFlag;
+    this->ServerPort = StatusDataObj.ServerPort;
+    this->ModularProductionEnabled = StatusDataObj.ModularProductionEnabled;
 
-    Obj.ActuatorModuleNumber = StatusDataObj.ActuatorModuleNumber;
-    Obj.ActuatorPartNumber = StatusDataObj.ActuatorPartNumber;
-    Obj.ActuatorSerialNumber = StatusDataObj.ActuatorSerialNumber;
-    Obj.ActuatorVersion = StatusDataObj.ActuatorVersion;
+    this->ActuatorModuleNumber = StatusDataObj.ActuatorModuleNumber;
+    this->ActuatorPartNumber = StatusDataObj.ActuatorPartNumber;
+    this->ActuatorSerialNumber = StatusDataObj.ActuatorSerialNumber;
+    this->ActuatorVersion = StatusDataObj.ActuatorVersion;
 
-    return Obj;
+    return *this;
 }
 
 
