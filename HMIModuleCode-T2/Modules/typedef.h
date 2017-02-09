6216d8a5f31dc9099a8f46694db3e042a137e113
Wang <JerryW.Wang@Emerson.com>
2017-02-09 18:20:18 +0800

    Added typedef.h

---
+++ HMIModuleCode-T2/Modules/typedef.h
@@ -0,0 +1,7 @@
+#ifndef TYPEDEF_H
+#define TYPEDEF_H
+#include <QDebug>
+#define UNUSED(x)   (void)x
+#define DEBUG_PRINT (qDebug() <<"Debug Info:" << __FILE__ << __FUNCTION__ << __LINE__ << ": ")
+
+#endif // TYPEDEF_H
